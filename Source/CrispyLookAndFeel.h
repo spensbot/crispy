4a428f171c7bfef7c339065d0d2085330e3e902c
Spenser Saling <spenser0saling@gmail.com>
2020-04-30 08:07:55 -0700

    Add montserrat Regular as the default Font

--- Source/CrispyLookAndFeel.h
+++ Source/CrispyLookAndFeel.h
@@ -51,12 +51,6 @@ public:
         g.setColour (Colours::white);
         g.fillPath (p);
     }
-    
-    //==============================================================================
-//    Font getTextButtonFont (TextButton&, int buttonHeight) override
-//    {
-//        return { jmin (16.0f, buttonHeight * 0.6f) };
-//    }
 
     void drawButtonBackground (Graphics& g,
                                                Button& button,
@@ -66,42 +60,6 @@ public:
     {
         auto cornerSize = 6.5567f;
         auto bounds = button.getLocalBounds().toFloat().reduced (0.5f, 0.5f);
-//
-//        auto baseColour = backgroundColour.withMultipliedSaturation (button.hasKeyboardFocus (true) ? 1.3f : 0.9f)
-//                                          .withMultipliedAlpha (button.isEnabled() ? 1.0f : 0.5f);
-//
-//        if (shouldDrawButtonAsDown || shouldDrawButtonAsHighlighted)
-//            baseColour = baseColour.contrasting (shouldDrawButtonAsDown ? 0.2f : 0.05f);
-//
-//        g.setColour (baseColour);
-//
-//        auto flatOnLeft   = button.isConnectedOnLeft();
-//        auto flatOnRight  = button.isConnectedOnRight();
-//        auto flatOnTop    = button.isConnectedOnTop();
-//        auto flatOnBottom = button.isConnectedOnBottom();
-//
-//        if (flatOnLeft || flatOnRight || flatOnTop || flatOnBottom)
-//        {
-//            Path path;
-//            path.addRoundedRectangle (bounds.getX(), bounds.getY(),
-//                                      bounds.getWidth(), bounds.getHeight(),
-//                                      cornerSize, cornerSize,
-//                                      ! (flatOnLeft  || flatOnTop),
-//                                      ! (flatOnRight || flatOnTop),
-//                                      ! (flatOnLeft  || flatOnBottom),
-//                                      ! (flatOnRight || flatOnBottom));
-//
-//            g.fillPath (path);
-//
-//            g.setColour (button.findColour (ComboBox::outlineColourId));
-//            g.strokePath (path, PathStrokeType (1.0f));
-//        }
-//        else
-//        {
-//            g.fillRoundedRectangle (bounds, cornerSize);
-//
-//
-//        }
         
         if (button.getToggleState()){
             g.setColour (colourAccent);
